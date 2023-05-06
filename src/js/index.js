212084a43b03862045427f399f75cea632ef0a4e
afbeltranv <124716013+afbeltranv@users.noreply.github.com>
2023-05-06 02:13:51 +0000

    primer commit

--- src/js/index.js
+++ src/js/index.js
@@ -8,5 +8,27 @@ import "../styles/index.css";
 //import your own components
 import Home from "./component/home.jsx";
 
+
+const cronometro()=>{
+let segundos=0;
+let decaSegundos=0;
+let hectoSegundos=0;
+let kiloSegundos=0;
+let megaSegundos=0;
+let gigaSegundos=0;
+
+if (segundos<9){
+ segundos++;
+} else{
+    segundos=0
+    decaSegundos++;
+}
+if (decaSegundos==10){
+    decaSegundos=0;
+    hectoSegundos++;
+
+}
+
+}
 //render your react application
 ReactDOM.render(<Home />, document.querySelector("#app"));
